e7c20410f720b8aa21ea1a1abf4ee59dee5f9062
Your Name <20dy3@queensu.ca>
2022-11-08 12:08:10 -0500

    real main file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# Triangle strips
+# Convex hull
 #
-# Usage: python main.py file_of_triangles
+# Usage: python main.py [-d] file_of_points
 #
 # You can press ESC in the window to exit.
 #
@@ -15,13 +15,13 @@ try: # PyOpenGL
   from OpenGL.GL import *
 except:
   print( 'Error: PyOpenGL has not been installed.' )
-  sys.exit(0)
+  sys.exit(1)
 
 try: # GLFW
   import glfw
 except:
   print( 'Error: GLFW has not been installed.' )
-  sys.exit(0)
+  sys.exit(1)
 
 
 
@@ -29,126 +29,83 @@ except:
 
 window = None
 
-windowWidth  = 1000 # window dimensions
-windowHeight = 1000
+windowWidth  = 800 # window dimensions
+windowHeight = 800
 
-minX = None # range of vertices
+minX = None # range of points
 maxX = None
 minY = None
 maxY = None
 
-r  = 0.008 # point radius as fraction of window size
+r  = 0.01 # point radius as fraction of window size
 
-allVerts = [] # all triangle vertices
+numAngles = 32
+thetas = [ i/float(numAngles)*2*3.14159 for i in range(numAngles) ] # used for circle drawing
+
+allPoints = [] # list of points
 
 lastKey = None  # last key pressed
 
-showForwardLinks = True
+discardPoints = False
 
-
-
-# Triangle
+# Point
 #
-# A Triangle stores its three vertices and pointers to any adjacent triangles.
+# A Point stores its coordinates and pointers to the two points beside
+# it (CW and CCW) on its hull.  The CW and CCW pointers are None if
+# the point is not on any hull.
 #
-# For debugging, you can set the 'highlight1' and 'highlight2' flags
-# of a triangle.  This will cause the triangle to be highlighted when
-# it's drawn.
+# For debugging, you can set the 'highlight' flag of a point.  This
+# will cause the point to be highlighted when it's drawn.
+
+class Point(object):
+
+    def __init__( self, coords ):
+
+      self.x = float( coords[0] ) # coordinates
+      self.y = float( coords[1] )
+
+      self.ccwPoint = None # point CCW of this on hull
+      self.cwPoint  = None # point CW of this on hull
+
+      self.highlight = False # to cause drawing to highlight this point
 
 
-class Triangle(object):
-
-    nextID = 0
-
-    def __init__( self, verts ):
-
-      self.verts   = verts # 3 vertices.  Each is an index into the 'allVerts' global.
-      self.adjTris = [] # adjacent triangles
-
-      self.nextTri = None  # next triangle on strip
-      self.prevTri = None  # previous triangle on strip
-
-      self.highlight1 = False # to cause drawing to highlight this triangle in colour 1
-      self.highlight2 = False # to cause drawing to highlight this triangle in colour 2
-
-      self.centroid = ( sum( [allVerts[i][0] for i in self.verts] ) / len(self.verts),
-                        sum( [allVerts[i][1] for i in self.verts] ) / len(self.verts) )
-
-      self.id = Triangle.nextID
-      Triangle.nextID += 1
-
-
-    # String representation of this triangle
-    
     def __repr__(self):
-        return 'tri-%d' % self.id
+      return 'pt(%g,%g)' % (self.x, self.y)
 
 
-    # Draw this triangle
-    
-    def draw(self):
+    def drawPoint(self):
 
-        # Highlight with yellow fill
-
-        if self.highlight1 or self.highlight2:
-
-            if self.highlight1:
-                glColor3f( 0.9, 0.9, 0.4 ) # dark yellow
-            else:
-                glColor3f( 1, 1, 0.8 ) # light yellow
-
-            glBegin( GL_POLYGON )
-            for i in self.verts:
-                glVertex2f( allVerts[i][0], allVerts[i][1] )
-            glEnd()
-
-        # Outline the triangle
-
-        glColor3f( 0, 0, 0 )
-        glBegin( GL_LINE_LOOP )
-        for i in self.verts:
-            glVertex2f( allVerts[i][0], allVerts[i][1] )
-        glEnd()
-
-
-    # Draw edges to next and previous triangle on the strip
-
-    def drawPointers(self):
-
-        if showForwardLinks and self.nextTri:
-            glColor3f( 0, 0, 1 )
-            drawArrow( self.centroid[0], self.centroid[1], 
-                       self.nextTri.centroid[0], self.nextTri.centroid[1] )
-
-        if not showForwardLinks and self.prevTri:
-            glColor3f( 1, 0, 0 )
-            drawArrow( self.centroid[0], self.centroid[1], 
-                       self.prevTri.centroid[0], self.prevTri.centroid[1] )
-
-        if not self.nextTri and not self.prevTri: # no links.  Draw a dot.
-            if showForwardLinks:
-                glColor3f( 0, 0, 1 )
-            else:
-                glColor3f( 1, 0, 0 )
-            glBegin( GL_POLYGON )
-            for i in range(100):
-                theta = 3.14159 * i/50.0
-                glVertex2f( self.centroid[0] + 0.5 * r * math.cos(theta), self.centroid[1] + 0.5 * r * math.sin(theta) ) 
-            glEnd()
-
-
-    # Determine whether this triangle contains a point
-    
-    def containsPoint( self, pt ):
-
-        return (turn( allVerts[self.verts[0]], allVerts[self.verts[1]], pt ) == LEFT_TURN and
-                turn( allVerts[self.verts[1]], allVerts[self.verts[2]], pt ) == LEFT_TURN and
-                turn( allVerts[self.verts[2]], allVerts[self.verts[0]], pt ) == LEFT_TURN)
+      # Highlight with yellow fill
       
+      if self.highlight:
+          glColor3f( 0.9, 0.9, 0.4 )
+          glBegin( GL_POLYGON )
+          for theta in thetas:
+              glVertex2f( self.x+r*math.cos(theta), self.y+r*math.sin(theta) )
+          glEnd()
+
+      # Outline the point
+      
+      glColor3f( 0, 0, 0 )
+      glBegin( GL_LINE_LOOP )
+      for theta in thetas:
+          glVertex2f( self.x+r*math.cos(theta), self.y+r*math.sin(theta) )
+      glEnd()
+
+      # Draw edges to next CCW and CW points.
+
+      if self.ccwPoint:
+        glColor3f( 0, 0, 1 )
+        drawArrow( self.x, self.y, self.ccwPoint.x, self.ccwPoint.y )
+
+      if self.cwPoint:
+        glColor3f( 1, 0, 0 )
+        drawArrow( self.x, self.y, self.cwPoint.x, self.cwPoint.y )
 
 
 
-# Draw an arrow between two points.
+# Draw an arrow between two points, offset a bit to the right
 
 def drawArrow( x0,y0, x1,y1 ):
 
@@ -160,11 +117,11 @@ def drawArrow( x0,y0, x1,y1 ):
     vpx = -vy             # unit direction perpendicular to (vx,vy)
     vpy = vx
 
-    xa = x0 + 0.15*r*vx # arrow tail
-    ya = y0 + 0.15*r*vy
+    xa = x0 + 1.5*r*vx - 0.4*r*vpx # arrow tail
+    ya = y0 + 1.5*r*vy - 0.4*r*vpy
 
-    xb = x1 - 0.15*r*vx # arrow head
-    yb = y1 - 0.15*r*vy
+    xb = x1 - 1.5*r*vx - 0.4*r*vpx # arrow head
+    yb = y1 - 1.5*r*vy - 0.4*r*vpy
 
     xc = xb - 2*r*vx + 0.5*r*vpx # arrow outside left
     yc = yb - 2*r*vy + 0.5*r*vpy
@@ -174,10 +131,10 @@ def drawArrow( x0,y0, x1,y1 ):
 
     glBegin( GL_LINES )
     glVertex2f( xa, ya )
-    glVertex2f( 0.5*(xc+xd), 0.5*(yc+yd) )
+    glVertex2f( xb, yb )
     glEnd()
 
-    glBegin( GL_LINE_LOOP )
+    glBegin( GL_POLYGON )
     glVertex2f( xb, yb )
     glVertex2f( xc, yc )
     glVertex2f( xd, yd )
@@ -193,7 +150,7 @@ COLLINEAR  = 3
 
 def turn( a, b, c ):
 
-    det = (a[0]-c[0]) * (b[1]-c[1]) - (b[0]-c[0]) * (a[1]-c[1])
+    det = (a.x-c.x) * (b.y-c.y) - (b.x-c.x) * (a.y-c.y)
 
     if det > 0:
         return LEFT_TURN
@@ -203,44 +160,62 @@ def turn( a, b, c ):
         return COLLINEAR
 
 
-
-# ================================================================
-# ================================================================
-# ================================================================
-
-# Build a set of triangle strips that cover all of the given
-# triangles.  The goal is to make the strips as long as possible
-# (i.e. to have the fewest strip that cover all triangles).
+# Build a convex hull from a set of point
 #
-# Follow the instructions in A2.txt.
-#
-# This function does not return anything.  The strips are formed by
-# modifying the 'nextTri' and 'prevTri' pointers in each triangle.
+# Use the method described in class
 
 
-def buildTristrips( triangles ):
+def buildHull( points ):
 
-    count = 0
-
-    # [YOUR CODE HERE]
+    # Handle base cases of two or three points
     #
-    # Increment 'count' every time you *start* a new triStrip.
+    # [YOUR CODE HERE]
 
-    print( 'Generated %d tristrips' % count )
+    # Handle recursive case.
+    #
+    # After you get the hull-merge working, do the following: For each
+    # point that was removed from the convex hull in a merge, set that
+    # point's CCW and CW pointers to None.  You'll see that the arrows
+    # from interior points disappear after you do this.
+    #
+    # [YOUR CODE HERE]
 
+    # You can do the following to help in debugging.  This highlights
+    # all the points, then shows them, then pauses until you press
+    # 'p'.  While paused, you can click on a point and its coordinates
+    # will be printed in the console window.  If you are using an IDE
+    # in which you can inspect your variables, this will help you to
+    # identify which point on the screen is which point in your data
+    # structure.
+    #
+    # This is good to do, for example, after you have recursively
+    # built two hulls, to see that the two hulls look right.
+    #
+    # This can also be done immediately after you have merged to hulls
+    # ... again, to see that the merged hull looks right.
+    #
+    # Always after you have inspected things, you should remove the
+    # highlighting from the points that you previously highlighted.
 
-# ================================================================
-# ================================================================
-# ================================================================
+    for p in points:
+        p.highlight = True
+    display(wait=True)
 
+    # At the very end of buildHull(), you should display the result
+    # after every merge, as shown below.  This call to display() does
+    # not pause.
+    
+    display()
 
-
-# Set up the display and draw the current image
+  
 
 windowLeft   = None
 windowRight  = None
 windowTop    = None
 windowBottom = None
+
+
+# Set up the display and draw the current image
 
 def display( wait=False ):
 
@@ -262,29 +237,17 @@ def display( wait=False ):
     glMatrixMode( GL_MODELVIEW )
     glLoadIdentity()
 
-    if maxX-minX > maxY-minY: # wider point spread in x direction
-        windowLeft = -0.1*(maxX-minX)+minX
-        windowRight = 1.1*(maxX-minX)+minX
-        windowBottom = windowLeft
-        windowTop    = windowRight
-    else: # wider point spread in y direction
-        windowTop    = -0.1*(maxY-minY)+minY
-        windowBottom = 1.1*(maxY-minY)+minY
-        windowLeft   = windowBottom
-        windowRight  = windowTop
+    windowLeft   = -0.1*(maxX-minX)+minX
+    windowRight  =  1.1*(maxX-minX)+minX
+    windowTop    =  1.1*(maxY-minY)+minY
+    windowBottom = -0.1*(maxY-minY)+minY
 
     glOrtho( windowLeft, windowRight, windowBottom, windowTop, 0, 1 )
 
-    # Draw triangles
+    # Draw points and hull
 
-    for tri in allTriangles:
-        tri.draw()
-
-    # Draw pointers.  Do this *after* the triangles (above) so that the
-    # triangle drawing doesn't overlay the pointers.
-
-    for tri in allTriangles:
-        tri.drawPointers()
+    for p in allPoints:
+        p.drawPoint()
 
     # Show window
 
@@ -300,6 +263,8 @@ def display( wait=False ):
         lastKey = None
         while lastKey != 80: # wait for 'p'
             glfw.wait_events()
+            if glfw.window_should_close( window ):
+              sys.exit(0)
             display()
 
         sys.stderr.write( '\r                     \r' )
@@ -312,14 +277,12 @@ def display( wait=False ):
 
 def keyCallback( window, key, scancode, action, mods ):
 
-    global lastKey, showForwardLinks
+    global lastKey
     
     if action == glfw.PRESS:
     
-        if key == glfw.KEY_ESCAPE: # quit upon ESC
-            sys.exit(0)
-        elif key == ord('F'): # toggle forward/backward link display
-            showForwardLinks = not showForwardLinks
+        if key == glfw.KEY_ESCAPE:      # quit upon ESC
+            glfw.set_window_should_close( window, GL_TRUE )
         else:
             lastKey = key
 
@@ -350,118 +313,19 @@ def mouseButtonCallback( window, btn, action, keyModifiers ):
         wx = (x-0)/float(windowWidth)  * (windowRight-windowLeft) + windowLeft
         wy = (windowHeight-y)/float(windowHeight) * (windowTop-windowBottom) + windowBottom
 
-        selectedTri = None
-        for tri in allTriangles:
-            if tri.containsPoint( [wx, wy] ):
-                selectedTri = tri
-                break
+        minDist = windowRight-windowLeft
+        minPoint = None
+        for p in allPoints:
+            dist = math.sqrt( (p.x-wx)*(p.x-wx) + (p.y-wy)*(p.y-wy) )
+            if dist < r and dist < minDist:
+                minDist = dist
+                minPoint = p
 
-        # print triangle, toggle its highlight1, and toggle the highlight2s of its adjacent triangles
+        # print point and toggle its highlight
 
-        if selectedTri:
-            selectedTri.highlight1 = not selectedTri.highlight1
-            print( '%s with adjacent %s' % (selectedTri, repr(selectedTri.adjTris)) )
-            for t in selectedTri.adjTris:
-                t.highlight2 = not t.highlight2
-
-
-# Read triangles from a file
-
-def readTriangles( f ):
-
-    global allVerts
-    
-    errorsFound = False
-    
-    lines = f.readlines()
-
-    # Read the vertices
-    
-    numVerts = int( lines[0] )
-    allVerts = [ [float(c) for c in line.split()] for line in lines[1:numVerts+1] ]
-
-    # Check that the vertices are valid
-
-    for l,v in enumerate(allVerts):
-        if len(v) != 2:
-            print( 'Line %d: vertex does not have two coordinates.' % (l+2) )
-            errorsFound = True
-
-    # Read the triangles
-
-    numTris = int( lines[numVerts+1] )
-    triVerts =  [ [int(v) for v in line.split()] for line in lines[numVerts+2:] ]
-
-    # Check that the triangle vertices are valid
-
-    for l,tvs in enumerate(triVerts):
-        if len(tvs) != 3:
-            print( 'Line %d: triangle does not have three vertices.' % (l+2+numVerts) )
-            errorsFound = True
-        else:
-            for v in tvs:
-                if v < 0 or v >= numVerts:
-                    print( 'Line %d: Vertex index is not in range [0,%d].' % (l+2+numVerts,numVerts-1) )
-                    errorsFound = True
-
-    # Build triangles
-
-    tris = []
-
-    for tvs in triVerts:
-        theseVerts = tvs
-        if turn( allVerts[tvs[0]], allVerts[tvs[1]], allVerts[tvs[2]] ) != COLLINEAR:
-          tris.append( Triangle( tvs ) ) # (don't include degenerate triangles)
-
-    # For each triangle, find and record its adjacent triangles
-    #
-    # This would normally take O(n^2) time if done by brute force, so
-    # we'll exploit Python's hashed dictionary keys.
-
-    if False:
-
-        for tri in tris: # brute force
-            adjTris = []
-            for i in range(3):
-                v0 = tri.verts[i % 3]
-                v1 = tri.verts[(i+1) % 3]
-                for tri2 in tris:
-                    for j in range(3):
-                        if v1 == tri2.verts[j % 3] and v0 == tri2.verts[(j+1) % 3]:
-                            adjTris.append( tri2 )
-                    if len(adjTris) == 3:
-                        break
-            tri.adjTris = adjTris
-
-    else: # hashing
-      
-        edges = {}
-
-        for tri in tris:
-            for i in range(3):
-                v0 = tri.verts[i % 3]
-                v1 = tri.verts[(i+1) % 3]
-                key = '%f-%f' % (v0,v1)
-                edges[key] = tri
-
-        for tri in tris:
-            adjTris = []
-            for i in range(3):
-                v1 = tri.verts[i % 3] # find a reversed edge of an adjacent triangle
-                v0 = tri.verts[(i+1) % 3]
-                key = '%f-%f' % (v0,v1)
-                if key in edges:
-                    adjTris.append( edges[key] )
-                if len(adjTris) == 3:
-                    break
-            tri.adjTris = adjTris
-
-    print( 'Read %d points and %d triangles' % (numVerts,numTris) )
-
-    if errorsFound:
-        return []
-    else:
-        return tris
+        if minPoint:
+            minPoint.highlight = not minPoint.highlight
+            print( minPoint )
 
         
     
@@ -469,7 +333,7 @@ def readTriangles( f ):
 
 def main():
 
-    global window, allTriangles, minX, maxX, minY, maxY, r
+    global window, allPoints, minX, maxX, minY, maxY, r, discardPoints
     
     # Check command-line args
 
@@ -479,8 +343,9 @@ def main():
 
     args = sys.argv[1:]
     while len(args) > 1:
-        # if args[0] == '-x':
-        #     pass
+        print( args )
+        if args[0] == '-d':
+            discardPoints = not discardPoints
         args = args[1:]
 
     # Set up window
@@ -489,7 +354,7 @@ def main():
         print( 'Error: GLFW failed to initialize' )
         sys.exit(1)
 
-    window = glfw.create_window( windowWidth, windowHeight, "Assignment 2", None, None )
+    window = glfw.create_window( windowWidth, windowHeight, "Assignment 1", None, None )
 
     if not window:
         glfw.terminate()
@@ -502,20 +367,17 @@ def main():
     glfw.set_window_size_callback( window, windowReshapeCallback )
     glfw.set_mouse_button_callback( window, mouseButtonCallback )
 
-    # Read the triangles.  This also fills in the global 'allVerts'.
+    # Read the points
 
     with open( args[0], 'rb' ) as f:
-        allTriangles = readTriangles( f )
-
-    if allTriangles == []:
-        return
+      allPoints = [ Point( line.split(b' ') ) for line in f.readlines() ]
 
     # Get bounding box of points
 
-    minX = min( p[0] for p in allVerts )
-    maxX = max( p[0] for p in allVerts )
-    minY = min( p[1] for p in allVerts )
-    maxY = max( p[1] for p in allVerts )
+    minX = min( p.x for p in allPoints )
+    maxX = max( p.x for p in allPoints )
+    minY = min( p.y for p in allPoints )
+    maxY = max( p.y for p in allPoints )
 
     # Adjust point radius in proportion to bounding box
     
@@ -524,14 +386,19 @@ def main():
     else:
         r *= maxY-minY
 
+    # Sort by increasing x.  For equal x, sort by increasing y.
+    
+    allPoints.sort( key=lambda p: (p.x,p.y) )
+
     # Run the code
     
-    buildTristrips( allTriangles )
+    buildHull( allPoints )
 
-    # Show result and wait to exit
+    # Wait to exit
 
-    display( wait=True )
-    
+    while not glfw.window_should_close( window ):
+        glfw.wait_events()
+
     glfw.destroy_window( window )
     glfw.terminate()
     
